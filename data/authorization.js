27c58ba5e870eca82cc89925f04096eec3eff303
aklex <aleksey.kabanov@gmail.com>
2015-06-18 21:57:18 +0300

    Check authorization, add link menu item

--- data/authorization.js
+++ data/authorization.js
@@ -2,16 +2,17 @@ self.port.on("get-user", getFirstPara);
 function getFirstPara(){
 	console.log("In script authorization....!!!!");
 	var elements = document.getElementsByClassName("dropdown-toggle user-menu");
+	console.log(elements.length);
 	var authoriz;
 //	user = window.user.authorized;
 	if (elements.length > 0){
 		authoriz = "yes";
 		console.log("Aauthorization yes....!!!!");
-		self.port.emit("first", authoriz); 
+		self.port.emit("my-script-response", authoriz); 
 	}
 	else {
 		authoriz = "no";
 		console.log("Authorization no....!!!!");
-		self.port.emit("first", authoriz); 
+		self.port.emit("my-script-response", authoriz); 
 	}
 }
--- data/reset-panel-storage.js
+++ data/reset-panel-storage.js
@@ -1,10 +1,3 @@
 self.port.on("detach", function() {
-//  Storage.clear();
-//  Storage.removeItem("download-browser-extension-dismiss");
-	if (window.localStorage.getItem("download-browser-extension-dismiss")){
-		window.localStorage.removeItem("download-browser-extension-dismiss");
-		self.postMessage("detach: remove local storage!!!!!!!!!");
-	}
-	self.postMessage("detach: nothing remove......!!!!!!!!!");
-  //Storage.removeItem();
+	self.postMessage("detach: remove......!!!!!!!!!");
 });
